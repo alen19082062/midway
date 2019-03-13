2fca977e4001be6b904bf9271ab8fb2da8d7893e
fengjy <fengjy@cy-life.com>
2019-03-13 19:39:50 +0800

    add nacos config

---
+++ examples/nacos-config-springboot/src/main/java/com/gg/demo_web/DemoController.java
@@ -0,0 +1,71 @@
+package com.gg.demo_web;
+
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@NacosPropertySource(dataId = "web-config", autoRefreshed = true)
+public class DemoController {
+
+    private static int byeCount = 0 ;
+
+    // 注入配置文件上下文
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+
+    @RequestMapping("/hello/{name}")
+    public String hello(@PathVariable("name") String name) {
+        System.out.println("Running class full name : " + this.getClass().getCanonicalName());
+        System.out.println("sayHello() name : " + name );
+        return "hello : " + name ;
+    }
+
+    @NacosValue(value = "${nacos.web.propertie:12344}", autoRefreshed = true)
+    private String testProperties;
+
+    @GetMapping(value = {"/conf"})
+    public Map<String,Object> conf() {
+        System.out.println("Running class full name : " + this.getClass().getCanonicalName());
+        System.out.println("conf() testProperties = "  + testProperties );
+        Map<String,Object> map =  new HashMap();
+        map.put("nacos.web.propertie",testProperties);
+        // System.out.println("sayBye() properities : " + str );
+        return map ;
+    }
+
+    @GetMapping(value = {"/hi"})
+    public Map<String,String> hi() {
+        System.out.println("Running class full name : " + this.getClass().getCanonicalName());
+        // String value = applicationContext.getEnvironment().getProperty("user.name");
+        // String value = applicationContext.getEnvironment().getgetProperty("user.name");
+        Map<String,String> map = new HashMap<>();
+        String str = "" ;
+        byeCount++ ;
+        System.out.println("sayBye() byeCount = "  + byeCount );
+
+        LocalDateTime ldt=LocalDateTime.now();
+        DateTimeFormatter format=DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss SSS");
+        String timeStr=ldt.format(format);
+        map.put("now_time",timeStr);
+        map.put("time_stamp", "" + System.currentTimeMillis()) ;
+        map.put("click_count", "" + byeCount) ;
+
+        return map ;
+
+    }
+
+
+
+}
---
+++ examples/nacos-config-springboot/src/main/java/com/gg/demo_web/DemoWebApplication.java
@@ -0,0 +1,29 @@
+package com.gg.demo_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class DemoWebApplication {
+
+    public static void main(String[] args) {
+        System.out.println("DemoWebApplication begin to start ");
+        ApplicationContext ctx = SpringApplication.run(DemoWebApplication.class, args);
+        System.out.println("DemoWebApplication start already ");
+
+        //所有的bean,参考：http://412887952-qq-com.iteye.com/blog/2314051
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        //String[] beanNames = ctx.getBeanNamesForAnnotation(RestController.class);//所有添加该注解的bean
+        System.out.println("====================== " );
+        System.out.println("proload beans count = " +  ctx.getBeanDefinitionCount());
+        int i = 0;
+        for (String str : beanNames) {
+            String print_str = "" + (++i) + " : beanName:" + str ;
+            System.out.println(print_str);
+        }
+        System.out.println("====================== " );
+    }
+
+}
+
